7505d63a9fd2e0a6edaeae8ec4502f4e401e51e7
tom <tgilholm@gmail.com>
2025-12-30 12:06:48 +0000

    Improved TransactionUtils with stream methods

--- app/src/main/java/com/example/budgetbuddy/utility/TransactionUtils.java
+++ app/src/main/java/com/example/budgetbuddy/utility/TransactionUtils.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.Contract;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 /**
@@ -22,11 +23,14 @@ import java.util.Map;
 public final class TransactionUtils
 {
     // Final class, no instantiation
-    private TransactionUtils() {}
+    private TransactionUtils()
+    {
+    }
 
 
     /**
      * Sorts a list of transactions by their date/time
+     *
      * @param transactions a list of <code>TransactionWithCategory</code> objects
      * @return the sorted list
      */
@@ -34,13 +38,14 @@ public final class TransactionUtils
     @Contract("_ -> param1")
     public static List<TransactionWithCategory> sortTransactions(@NonNull List<TransactionWithCategory> transactions)
     {
-        transactions.sort((o1, o2) ->
-                o2.transaction.getDateTime().compareTo(o1.transaction.getDateTime()));
+        transactions.sort((o1, o2) -> o2.transaction.getDateTime()
+                .compareTo(o1.transaction.getDateTime()));
         return transactions;
     }
 
     /**
      * Converts an <code>input</code> into a <code>RepeatDuration</code>
+     *
      * @param input a name in the enum
      * @return a <code>RepeatDuration</code> type
      */
@@ -67,91 +72,65 @@ public final class TransactionUtils
 
     /**
      * Gets the total spending of a list of <code>Transaction</code> objects
+     *
      * @param transactions a list of <code>Transaction</code> objects
      * @return the sum of all the amounts, as a <code>double</code>
      */
-    public static double getTotalSpend(List<TransactionWithCategory> transactions)
+    public static double getTotalSpend(@NonNull List<TransactionWithCategory> transactions)
     {
-        // todo convert to stream
-        double total = 0;
-        if (transactions != null)
-        {
-            for (TransactionWithCategory t : transactions)
-            {
-                Transaction transaction = t.transaction;
-                if (transaction.getType() == TransactionType.OUTGOING)
-                {
-                    total += transaction.getAmount();
-                }
-            }
-        }
-        return total;
+        /*
+        Takes the list of TransactionWithCategory objects & extracts the Transaction.
+        Filters to only use outgoing transactions
+        Gets the amount from each transaction, sums the amounts and returns the total
+         */
+        return transactions.stream()
+                .map(t -> t.transaction)
+                .filter(t -> t.getType() == TransactionType.OUTGOING)
+                .mapToDouble(Transaction::getAmount)
+                .sum();
     }
 
     /**
      * Calculates the remaining budget given a starting budget and a list of transactions
-     * @param start the <code>double</code> starting budget
+     *
+     * @param start        the <code>double</code> starting budget
      * @param transactions a list of <code>TransactionWithCategory</code> objects
      * @return a <code>double</code> sum of outgoing transactions
      */
-    public static double getBudgetRemaining(double start, List<TransactionWithCategory> transactions)
+    public static double getBudgetRemaining(double start, @NonNull List<TransactionWithCategory> transactions)
     {
-        // todo convert to stream
-        if (transactions == null)
-        {
-            return start;
-        }
+        /*
+        Takes the list of TransactionWithCategory objects & extracts the Transaction.
+        Filters to only use outgoing transactions
+        Adds the negated amount for outgoing transactions and adds the positive amounts for incoming ones
+        Returns the sum of these amounts
+         */
+        double change = transactions.stream()
+                .map(t -> t.transaction)
+                .mapToDouble(t -> t.getType() == TransactionType.OUTGOING ? -t.getAmount() : t.getAmount())
+                .sum();
 
-        double result = start;
-        for (TransactionWithCategory t : transactions)
-        {
-            Transaction transaction = t.transaction;
-            if (transaction.getType() == TransactionType.OUTGOING)
-            {
-                result -= transaction.getAmount();            // Subtract outgoings
-            } else
-            {
-                result += transaction.getAmount();            // Add incoming
-            }
-        }
-        return result;
+        // Return the start + the newly-calculated amount
+        // (Can result in negative numbers)
+        return start + change;
     }
 
 
     /**
      * Gets the sum of outgoing amounts from a list of <code>TransactionWithCategory</code> objects,
      * and aggregates them into a map of <code>Category</code> and <code>Double</code> objects
+     *
      * @param transactions a list of <code>TransactionWithCategory</code> objects
      * @return a <code>Map</code> of <code>Category</code> and <code>Double</code> objects with aggregated spending
      */
     @NonNull
-    public static Map<Category, Double> getCategoryTotals(List<TransactionWithCategory> transactions)
+    public static Map<Category, Double> getCategoryTotals(@NonNull List<TransactionWithCategory> transactions)
     {
         // todo convert to stream
-        Map<Category, Double> totalPerCategory = new HashMap<>();
-
-        // Checks if transactions is null
-        if (transactions == null)
-        {
-            return totalPerCategory;    // Returns empty HashMap if so
-        }
-
-        // Put the transactions into the Map
-        for (TransactionWithCategory t : transactions)
-        {
-            Transaction transaction = t.transaction;
-            Category category = t.category;
-            double amount = transaction.getAmount();
-
-            // Only handles "outgoing" transactions
-            if (transaction.getType() == TransactionType.OUTGOING)
-            {
-                // Uses Map.merge() instead of if-else statements
-                // Adds the amount to the total if the category already exists in the map
-                // Otherwise, adds the category to the map with amount as the value
-                totalPerCategory.merge(category, amount, Double::sum);
-            }
-        }
-        return totalPerCategory;
+        return transactions.stream()
+                .filter(t -> t.transaction.getType() == TransactionType.OUTGOING)   // Only consider outgoing values
+                .collect(Collectors.groupingBy(t -> t.category,                     // The "key"
+                        Collectors.summingDouble(t -> t.transaction.getAmount())    // The "amount", as the sum of the amounts
+                ));
     }
 }
